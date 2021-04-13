f9b2659257f0ea4d0485b30131b04f36c8d5b20c
sergio1599 <sergioq15@hotmail.com>
2021-04-13 12:39:54 -0500

    adding button with css styles

--- components/info/info.js
+++ components/info/info.js
@@ -3,13 +3,23 @@ import React from 'react';
 
 
 const Info = ({}) => {
+
     return (<div className={styles.info}>
+            <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300&display=swap" rel="stylesheet"/>
+            <div>
+                <label className={styles.text}>Ingrese el caracter:</label>
+            </div>
+            <div>
 
-            <div className={styles.group}>
-                <label>Ingrese el caracter:</label>
-                <br/>
-                <input className={styles.input} type="text" name="caracter" />
+            </div>
+            <div>
+                <input className={styles.input} type="text" name="caracter"/>
+            </div>
+            <div>
 
+            </div>
+            <div>
+                <button className={styles.button}>Calcular</button>
             </div>
         </div>
     );
