dd943d080288e6078d23cd85b7d1828f1e26ead4
sergio1599 <sergioq15@hotmail.com>
2021-04-13 12:54:04 -0500

    update grid from info

--- components/info/info.js
+++ components/info/info.js
@@ -10,13 +10,13 @@ const Info = ({}) => {
                 <label className={styles.text}>Ingrese el caracter:</label>
             </div>
             <div>
-
+                <label className={styles.text}>Convertido a binario:</label>
             </div>
             <div>
                 <input className={styles.input} type="text" name="caracter"/>
             </div>
             <div>
-
+                <p className={styles.text}>010001011</p>
             </div>
             <div>
                 <button className={styles.button}>Calcular</button>
