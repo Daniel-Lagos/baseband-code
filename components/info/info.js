959bfbd5aa7b3eb904defe5ca8abe7dc536a1f24
sergio1599 <sergioq15@hotmail.com>
2021-04-20 14:02:54 -0500

    implement function for convert the string to binary

--- components/info/info.js
+++ components/info/info.js
@@ -1,9 +1,13 @@
 import Styles from './info.module.css';
 import React from 'react';
+import Logic, {stringToBinary} from "../logic/logic";
 
 
 const Info = () => {
-
+    const handleInputChange = () =>{
+        //stringToBinary("a");
+        console.log(stringToBinary("a"));
+    }
   return (<div className={Styles.info}>
       <link
         href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300&display=swap"
@@ -11,14 +15,14 @@ const Info = () => {
       <div className={Styles.inputContent}>
         <div className={Styles.input}>
           <label className={Styles.text}>Ingrese el caracter:</label>
-          <input type="text" name="caracter" placeholder={'Digita un caracter'}/>
+          <input type="text" name="caracter" onChange={handleInputChange} placeholder={'Digita un caracter'}/>
         </div>
         <div className={Styles.input}>
           <label className={Styles.text}>Convertido a binario:</label>
           <label className={Styles.text}>010001011</label>
         </div>
       </div>
-      <button className={Styles.button}>Calcular</button>
+      <button className={Styles.button} onClick="stringToBinary()">Calcular</button>
     </div>
   );
 };
