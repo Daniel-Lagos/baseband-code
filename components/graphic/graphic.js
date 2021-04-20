b619eaf45eef34581f9ea8236fa1f004aa35209b
sergio1599 <sergioq15@hotmail.com>
2021-04-20 11:25:44 -0500

    adding charts.js and create component BarChart

--- components/graphic/graphic.js
+++ components/graphic/graphic.js
@@ -1,19 +1,32 @@
 import styles from './graphic.module.css';
 import React from 'react';
+import BarChart from "../BarChart/BarChart";
+
+
+
 
 const Graphic = () => {
 
-  return (<div className={styles.graphic}>
-      <h3>Gráfica 1</h3>
-      <div className={styles.pictureGraphic}><p>Acá iría la gráfica 📈</p></div>
-      <h3>Gráfica 2</h3>
-      <div className={styles.pictureGraphic}><p>Acá iría la gráfica 📈</p></div>
-      <h3>Gráfica 3</h3>
-      <div className={styles.pictureGraphic}><p>Acá iría la gráfica 📈</p></div>
-      <h3>Gráfica 4</h3>
-      <div className={styles.pictureGraphic}><p>Acá iría la gráfica 📈</p></div>
-    </div>
-  );
+
+    return (<div className={styles.graphic}>
+            <h3>Gráfica 1</h3>
+            <div className={styles.pictureGraphic}>
+                <BarChart/>
+            </div>
+            <h3>Gráfica 2</h3>
+            <div className={styles.pictureGraphic}>
+                <BarChart/>
+            </div>
+            <h3>Gráfica 3</h3>
+            <div className={styles.pictureGraphic}>
+                <BarChart/>
+            </div>
+            <h3>Gráfica 4</h3>
+            <div className={styles.pictureGraphic}>
+                <BarChart/>
+            </div>
+        </div>
+    );
 };
 
 export default Graphic;
